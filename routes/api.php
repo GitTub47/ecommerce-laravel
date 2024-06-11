49cfa654b9d92e4a38c276f451ba4f7e7fe8dc21
GitTub47 <nicholasdwikitanoto@gmail.com>
2024-06-12 01:07:58 +0700

    testing merge

--- routes/api.php
+++ routes/api.php
@@ -3,7 +3,7 @@
 use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+//testing merge
 Route::group([
     'middleware' => 'api',
     'prefix' => 'auth'
